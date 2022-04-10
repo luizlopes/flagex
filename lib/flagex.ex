ee6cd1c0d00c2c50d5ac108f57c6d78142c2bd52
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2022-04-10 01:15:19 -0300

    FeatureFlagsController calls Flagex module to reduce modules coupling

--- lib/flagex.ex
+++ lib/flagex.ex
@@ -6,4 +6,8 @@ defmodule Flagex do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias Flagex.FeatureFlag
+
+  defdelegate process_feature_flag(name, params), to: FeatureFlag, as: :process
 end
--- lib/flagex_web/controllers/feature_flags_controller.ex
+++ lib/flagex_web/controllers/feature_flags_controller.ex
@@ -1,12 +1,10 @@
 defmodule FlagexWeb.FeatureFlagsController do
   use FlagexWeb, :controller
 
-  alias Flagex.FeatureFlag
-
   action_fallback FlagexWeb.FallbackController
 
   def show(conn, %{"name" => name} = params) do
-    with {:ok, result} <- FeatureFlag.process(name, params) do
+    with {:ok, result} <- Flagex.process_feature_flag(name, params) do
       conn
       |> put_status(:ok)
       |> render("show.json", result: result)
