3da069d20af76b973155b4623f71bab92e104a0f
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2022-04-18 23:36:00 -0300

    Setup Coveralls to ignore Phoenix default files

--- lib/flagex/dtos/error.ex
+++ lib/flagex/dtos/error.ex
@@ -12,5 +12,4 @@ defmodule Flagex.Error do
   end
 
   def build_not_found_error, do: build(:not_found, "not found")
-  def build_invalid_id_format_error, do: build(:bad_request, "Invalid id format")
 end
